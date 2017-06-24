82e0678bcbe55dcf195e9a20cd822f6324cc6468
Alberto <Alberto@Gentilnucleo-PC>
2017-06-24 13:18:33 -0300

    Entidad persona: con sus campos, constructor y sobreescritura de equals

--- TpGentilcoreLopapa/src/entity/Persona.java
+++ TpGentilcoreLopapa/src/entity/Persona.java
@@ -1,10 +1,47 @@
 package entity;
 
 public class Persona {
+	private String dni;
 	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String contraseña;
+	private int categoria; //prom. direct     1-encargado 2-admin 3-usuario
 	
-	public static void main(String[] args) {
-		    System.out.println("Hola mundo");
-		  }
+	public String getDni() {
+		return dni; 	}
+	public void setDni(String dni) {
+		this.dni = dni;  }
 	
+	public String getNombre() {
+		return nombre; 	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre; }
+	
+	public String getApellido() {
+		return apellido; }
+	public void setApellido(String apellido) {
+		this.apellido = apellido;	}
+	
+	public String getUsuario() {
+		return usuario;  }
+	public void setUsuario(String usuario) {
+		this.usuario = usuario; }
+	
+	public String getContraseña() {
+		return contraseña;	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;	}
+	
+	Persona(){
+		this.setApellido(apellido);
+		this.setNombre(nombre);
+		this.setDni(dni);
+		this.setUsuario(usuario);
+		this.setContraseña(contraseña);		}
+	
+	@Override
+	public boolean equals(Object p){
+		return(p instanceof Persona) && (((Persona)p).getDni().equals(this.getDni()));
+				}
 }
