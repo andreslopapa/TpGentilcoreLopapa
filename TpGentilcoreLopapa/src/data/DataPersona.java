4678bed9f0550804e434974816db3cfa15a4ea53
alberto.gentilcore <alberto.gentilcore@ALBERTOGENTILCO.neoris.cxnetworks.net>
2017-07-12 14:38:17 -0300

    cree PersonaLogic y deberia hacer DataPersona y la conexion a la BD

---
+++ TpGentilcoreLopapa/src/data/DataPersona.java
@@ -0,0 +1,31 @@
+package data;
+
+import java.util.ArrayList;
+import java.security.KeyStore.ProtectionParameter;
+import java.sql.*;
+
+import business.entities.*;
+
+public class DataPersona {
+	
+	public ArrayList<Persona> getAll(){
+		Statement stmt = null;
+		ResultSet rs=null;
+	}
+
+	
+	/*
+	public Persona getByDni(){
+//
+//completar//////////////////////////////////////////////////////////////
+//		
+	}
+	
+	
+	public Persona add(){
+///
+//completar//////////////////////////////////////////////////////////////
+//		
+	}
+*/
+}
