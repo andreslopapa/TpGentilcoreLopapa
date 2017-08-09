0608da178e4b88fa586e2df3dd5f15b07f292f5a
alberto.gentilcore <alberto.gentilcore@ALBERTOGENTILCO.neoris.cxnetworks.net>
2017-08-09 10:06:47 -0300

    Ese inicio de sesion si que se puede ver

--- TpGentilcoreLopapa/src/ui/Desktop/VentanaPrincipal.java
+++ TpGentilcoreLopapa/src/ui/Desktop/VentanaPrincipal.java
@@ -32,10 +32,14 @@ import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseMotionAdapter;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 public class VentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textUsuario;
+	private JTextField textContrasenia;
 
 	/**
 	 * Launch the application.
@@ -74,7 +78,7 @@ public class VentanaPrincipal extends JFrame {
 				setLocation(point.x - x, point.y - y)  ;
 			}
 		});
-		contentPane.setBackground(Color.WHITE);
+		contentPane.setBackground(new Color(51, 51, 102));
 		contentPane.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);	
@@ -94,8 +98,8 @@ public class VentanaPrincipal extends JFrame {
 		
 
 		lblSistemaDeGestin.setBounds(0, 1, 742, 31);
-		lblSistemaDeGestin.setForeground(Color.WHITE);
-		lblSistemaDeGestin.setBackground(new Color(0, 51, 102));
+		lblSistemaDeGestin.setForeground(Color.DARK_GRAY);
+		lblSistemaDeGestin.setBackground(Color.WHITE);
 		lblSistemaDeGestin.setOpaque(true);
 		lblSistemaDeGestin.setFont(new Font("Calibri", Font.PLAIN, 12));
 		contentPane.add(lblSistemaDeGestin);
@@ -134,12 +138,13 @@ O)
 		lblCerrar.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCerrar.setForeground(Color.WHITE);
 		lblCerrar.setBounds(772, 1, 28, 31);
-		lblCerrar.setBackground(SystemColor.controlText);
+		lblCerrar.setBackground(Color.BLACK);
 		lblCerrar.setOpaque(true);
 		lblCerrar.setFont(new Font("Calibri", Font.PLAIN, 12));
 		contentPane.add(lblCerrar);
 		
 		JLabel labMinimizar = new JLabel("-");
+		labMinimizar.setFont(new Font("Calibri", Font.PLAIN, 12));
 		labMinimizar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -149,7 +154,7 @@ O)
 		labMinimizar.setHorizontalAlignment(SwingConstants.CENTER);
 		labMinimizar.setForeground(Color.WHITE);
 		labMinimizar.setBounds(743, 1, 28, 31);
-		labMinimizar.setBackground(SystemColor.controlText);
+		labMinimizar.setBackground(Color.DARK_GRAY);
 		labMinimizar.setOpaque(true);
 
 		contentPane.add(labMinimizar);
@@ -157,7 +162,60 @@ O)
 		
 		labMinimizar.setFocusable(false)  ; 
 		lblCerrar.setFocusable(false)  ; 
-		lblSistemaDeGestin.setFocusable(false)  ; 
+		lblSistemaDeGestin.setFocusable(false)  ;
+		
+		Panel panel_Gris_Derecho = new Panel();
+		panel_Gris_Derecho.setBackground(new Color(0, 51, 102));
+		panel_Gris_Derecho.setBounds(288, 33, 512, 417);
+		contentPane.add(panel_Gris_Derecho);
+		panel_Gris_Derecho.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setForeground(Color.WHITE);
+		lblUsuario.setBounds(68, 107, 43, 16);
+		lblUsuario.setFont(new Font("Calibri", Font.PLAIN, 14));
+		panel_Gris_Derecho.add(lblUsuario);
+		
+		textUsuario = new JTextField();
+		textUsuario.setBounds(67, 132, 348, 20);
+		panel_Gris_Derecho.add(textUsuario);
+		textUsuario.setColumns(10);
+		
+		JLabel lblContrasenia = new JLabel("Contrase\u00F1a");
+		lblContrasenia.setForeground(Color.WHITE);
+		lblContrasenia.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblContrasenia.setBounds(68, 196, 77, 14);
+		panel_Gris_Derecho.add(lblContrasenia);
+		
+		textContrasenia = new JTextField();
+		textContrasenia.setBounds(68, 221, 347, 20);
+		panel_Gris_Derecho.add(textContrasenia);
+		textContrasenia.setColumns(10);
+		
+		JLabel lblIngresar = new JLabel("Ingresar");
+		lblIngresar.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIngresar.setFont(new Font("Calibri", Font.BOLD, 14));
+		lblIngresar.setForeground(Color.WHITE);
+		lblIngresar.setBounds(343, 292, 72, 16);
+		panel_Gris_Derecho.add(lblIngresar);
+		
+		JLabel lblInicioSesion = new JLabel("Iniciar sesi\u00F3n");
+		lblInicioSesion.setFont(new Font("Calibri", Font.PLAIN, 18));
+		lblInicioSesion.setForeground(Color.WHITE);
+		lblInicioSesion.setBounds(68, 40, 122, 20);
+		panel_Gris_Derecho.add(lblInicioSesion);
+		
+		JLabel lblnoTieneUnaCuenta = new JLabel("\u00BFNo tiene una cuenta?");
+		lblnoTieneUnaCuenta.setFont(new Font("Calibri", Font.PLAIN, 11));
+		lblnoTieneUnaCuenta.setForeground(Color.WHITE);
+		lblnoTieneUnaCuenta.setBounds(68, 293, 107, 14);
+		panel_Gris_Derecho.add(lblnoTieneUnaCuenta);
+		
+		JLabel lblCreeUna = new JLabel("Cree una");
+		lblCreeUna.setFont(new Font("Calibri", Font.BOLD, 12));
+		lblCreeUna.setForeground(Color.WHITE);
+		lblCreeUna.setBounds(181, 294, 55, 14);
+		panel_Gris_Derecho.add(lblCreeUna);
 	//	button_Minimizar.setFocusable(false)  ; 
 	//	button_Cerrar.setFocusable(false) ; 
 
@@ -166,7 +224,7 @@ O)
 		
 		
 		
-	}
+	}/*
     public static class FrameDragListener extends MouseAdapter {
 
         private final JFrame frame;
@@ -188,5 +246,5 @@ O)
             Point currCoords = e.getLocationOnScreen();
             frame.setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
         }
-    }
+    }*/
 }
